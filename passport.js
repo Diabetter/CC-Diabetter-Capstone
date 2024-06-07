ad54a871ff13dd17fde8f6cc14745ac778da48c2
azharanas173 <azharanas173@gmail.com>
2024-06-08 06:34:23 +0700

    Add passport for login with Google

---
+++ passport.js
@@ -0,0 +1,39 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { getUserByGoogleId, createUser } from "../helpers/database.js";
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+},
+async (accessToken, refreshToken, profile, done) => {
+    try {
+        let user = await getUserByGoogleId(profile.id);
+        if (!user) {
+            user = await createUser({
+                googleId: profile.id,
+                email: profile.emails[0].value,
+                name: profile.displayName
+            });
+        }
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await getUserById(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
+
+export default passport;
