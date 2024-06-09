aac6dfff806f62ba542b770477bf6d05db2e8933
azharanas173 <azharanas173@gmail.com>
2024-06-09 15:37:58 +0700

    feat: moving from use mysql to firebase

--- app.js
+++ app.js
@@ -1,55 +1,18 @@
-import express from "express";
-import session from "express-session";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import fs from "node:fs";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import passport from "./passport.js";
-import AuthRoute from "./routes/auth.js";
-import pool, { createTableIfNotExists } from "./helpers/database.js";
-import { createLogDirectory } from "./helpers/createLogDir.js";
-import { error } from "node:console";
-
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import router from './routes/auth.js';
 
 dotenv.config();
-createLogDirectory();
 
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const publicDirectory = path.join(__dirname, "./public");
-const PORT = process.env.PORT || 8081;
-const accessLogStream = fs.createWriteStream( path.join(__dirname, "logs/access.log"), { flags: "a" });
+app.use(express.json())
+app.use(cookieParser())
 
-
-app.use(morgan("common", { stream: accessLogStream }));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(publicDirectory));
-
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: false,
-	saveUninitialized: false,
-}))
-// helper for login Google
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use("/auth", AuthRoute);
-
-createTableIfNotExists().then(() => {
-	app.listen(PORT, () => {
-		console.log(`Server is running on port ${PORT}...`);
-	});
-}).catch ((error) => {
-	console.error("Error starting server:", error);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}...`);
 });
 
-process.on('SIGINT', () => {
-	console.log("Received SIGINT signal. Gracefully shutting down...")
-	pool.end();
-	process.exit();
-});
+app.use(router);
---
+++ config/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from 'firebase/app';
+import dotenv from 'dotenv';
+import admin from 'firebase-admin';
+import { getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    signOut, 
+    sendEmailVerification, 
+    sendPasswordResetEmail } from 'firebase/auth';
+import serviceAccount from '../serviceAccountKey.json' assert { type: "json" };
+
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export { getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    admin };
