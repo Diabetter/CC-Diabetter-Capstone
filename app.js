bc6d91539c01cd7a2fd15f869ad0573cf7659e53
azharanas17 <azharanas173@gmail.com>
2024-06-17 15:29:07 +0000

    feat: cors for public access

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes/auth.js';
 
@@ -10,6 +11,11 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+	origin: '*',
+	methods: ['GET', 'POST'],
+	allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}...`);
